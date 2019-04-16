b9baad1ca65c384ebcab9d1aae0957bb0b5345e7
unknown <315299489@qq.com>
2019-04-16 21:21:04 +0800

    4.16

--- main2.py
+++ main2.py
@@ -3,7 +3,7 @@ import os
 import threading
 import time
 from adjacent import is_adjacent, adjacent_rate
-import tkinter as tk
+from tkinter import *
 from queue import Queue
 global keyboard_stream
 '''
@@ -26,19 +26,20 @@ def do_press(event):
     if not started:
         keyboard_stream.append([event.name, 'press',event.time])
         keyboard_pressed.append(event.name)
-        if event.name == 'space' or event.name == 'backspace':
+        if event.name in ['space','backspace','enter']:
             keyboard_stream.clear()
             keyboard_pressed.clear()
-
+        # for the judgement, add the time evaluation (only care about the press event)
         if len(keyboard_pressed) >= 4 and adjacent_rate(keyboard_pressed) > 2/3:
             print("alert!",keyboard_pressed[-1],keyboard_pressed[-2])
             started = True
             start_time = time.time() # mark the start time
 
-            stop_recording = threading.Timer(2.5, stop_and_process)
+            stop_recording = threading.Timer(2.0, stop_and_process)
             stop_recording.start()
     elif started:
         keyboard_record.append([event.name, 'press',event.time])
+        keyboard_pressed.append(event.name)
 
     #print(event.name, event.scan_code, event.time,"press")
 
@@ -50,8 +51,6 @@ def do_release(event):
     #print(event.name, event.scan_code, event.time,"release")
     #keyboard.write("α")
 
-def stop_and_process():
-    print("stop")
 
 def listen_keyboard():
     #keyboard.add_hotkey('ctrl+q', quit)
@@ -67,16 +66,28 @@ def listen_keyboard():
     print(keyboard_record)
     #print(raw)
 
+def stop_and_process():
+    print("stop")
+    GUI = threading.Thread(target=start_GUI)
+    GUI.start() 
+
+
 def start_GUI():
-    window = tk.Tk()
+    window = Tk()
     # 进入消息循环
     window.title('gpk')   #窗口标题
-    window.geometry('200x100')  #窗口尺寸
-
+    window.geometry('400x50')  #窗口尺寸
+    
+    results = "1. 2. 3. 4. 5."
+    options = Label(window, text = results, width = 30, height = 2,anchor=NW,font=("Arial",30))
+    options.pack()
     window.mainloop()
 
 if __name__ == '__main__':
-    listen = threading.Thread(target=listen_keyboard)
-    #GUI = threading.Thread(target=start_GUI)
-    listen.start()
-    #GUI.start() 
+    # listen = threading.Thread(target=listen_keyboard)
+   
+    # listen.start()
+    stop_recording = threading.Thread(target=stop_and_process)
+    stop_recording.start()
+
+    
