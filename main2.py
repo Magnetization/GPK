08c705305ea0a87c477a6e1410472b9c721e36b0
unknown <315299489@qq.com>
2019-04-18 21:10:39 +0800

    finish the naive frame

--- main2.py
+++ main2.py
@@ -6,6 +6,7 @@ import time
 from adjacent import is_adjacent, adjacent_rate
 from tkinter import *
 from queue import Queue
+from predict import predict
 global keyboard_stream
 '''
 [keyname, event, time]
@@ -14,7 +15,9 @@ keyboard_stream = []
 keyboard_record = []
 keyboard_pressed = []
 previous_space_time = 0
-started = False
+started = False  # mark that a gesture is on
+processed = False  # mark it's in the process step
+
 start_time = 0
 def convert_to_ms(time):
     return int(time*1000) % 100000
@@ -25,12 +28,13 @@ def do_press(event):
     global started
     global start_time
     if not started:
+        
         keyboard_stream.append([event.name, 'press',event.time])
         keyboard_pressed.append(event.name)
         if event.name in ['space','backspace','enter']:
             keyboard_stream.clear()
             keyboard_pressed.clear()
-        # for the judgement, add the time evaluation (only care about the press event)
+            # for the judgement, add the time evaluation (only care about the press event)
         if len(keyboard_pressed) >= 4 and adjacent_rate(keyboard_pressed) > 2/3:
             #print("alert!",keyboard_pressed[-1],keyboard_pressed[-2])
             print("alert!", keyboard_pressed)
@@ -39,10 +43,22 @@ def do_press(event):
 
             stop_recording = threading.Timer(2.0, stop_and_process)
             stop_recording.start()
-    elif started:
-        keyboard_record.append([event.name, 'press',event.time])
-        keyboard_pressed.append(event.name)
-
+        
+    else:
+        if not processed:
+            keyboard_record.append([event.name, 'press',event.time])
+            keyboard_pressed.append(event.name)
+        else:
+            if event.name in ['1','2','3','4','5','`'] and len(results) != 0:
+                if event.name == '`':
+                    #pass
+                    reset()
+                else:
+                    for i in range(len(keyboard_pressed)):
+                        keyboard.press('backspace')
+                    keyboard.press('backspace')
+                    keyboard.write(results[int(event.name) - 1])
+                    reset()
     #print(event.name, event.scan_code, event.time,"press")
 
 def do_release(event):
@@ -73,18 +89,43 @@ def stop_and_process():
     GUI = threading.Thread(target=start_GUI)
     GUI.start() 
 
+def reset():
+    
+    keyboard_stream.clear()
+    keyboard_pressed.clear()
+    keyboard_record.clear()
+    global started
+    global processed
+    started = False    
+    processed = False
+    window.destroy() # can't use this, try another idea?
 
 def start_GUI():
+    # entering the processing phrase
+    global window
+    global results
     window = Tk()
     # 进入消息循环
     window.title('gpk')   #窗口标题
     window.geometry('400x50')  #窗口尺寸
     
-    results = ["α","β","Ω","π","μ"]
+    print("showing window")
+    #results = ["α","β","Ω","π","μ"]
+    results = predict(keyboard_pressed)
     options = "1." + results[0] + "  2." + results[1] + "  3." + results[2] + "  4." + results[3] + "  5." + results[4]
     options = Label(window, text = options, width = 30, height = 2,anchor=NW,font=("Arial",30))
     options.pack()
+    raise_window_up(window)
+    global processed
+    processed = True 
+
     window.mainloop()
+
+def raise_window_up(window):
+    window.attributes('-topmost', 1)
+def raise_window_down(window):
+    window.attributes('-topmost', 0)
+
 
 if __name__ == '__main__':
     listen = threading.Thread(target=listen_keyboard)
--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 import numpy as np
-from fastdtw import fastdtw
+from fastdtw import dtw
 import math
 symbols_tobe_matched = ["α", "β", "Ω" , "π","μ" ,  "λ",  "ζ" ,"θ", "γ", "η" ,"σ" ,"∑", "∫"  , "Δ" , "φ" , "⊂", "⊃", "∪", "∩", "∞", "÷", "×", "√", "ε", "£"]
 possibilities_matrix = np.array([
@@ -17,6 +17,8 @@ possibilities_matrix = np.array([
                 [0.0, 0.169683280892, 0.027333056963, 0.026261887559, 0.0, 0.00925995172123, 0.126814523724, 0.357931278937, ],
                 [0.0, 0.119499001708, 0.0937800037818, 0.319820552786, 0.0, 0.0712093216275, 0.0978846711553, 0.0515555780695, ]
             ])
+'''
+below are the original vectors, without normalization
 clustered_centroids = np.array([
             [-77.8, 25],                 #0
             [-42.64516129, 25.32258065], #1
@@ -31,6 +33,21 @@ clustered_centroids = np.array([
             [42.13888889, -25.44444444], #10
             [77.81818182, -25]           #11
         ])
+'''
+clustered_centroids = np.array([
+            [-0.9520540226789245, 0.3059299558736904],
+            [-0.8598368981931307, 0.5105688087864514],
+            [-0.44323004728450727, 0.8964079011165471],
+            [0.5685915015131715, 0.8226200243168149],
+            [0.8906589323925126, 0.45467204241016385],
+            [-1.0, 0.0],
+            [1.0, 0.0],
+            [-0.8888031673925841, -0.45828913322585996],
+            [-0.614320952843256, -0.7890562507817513],
+            [0.42539177031743186, -0.9050093047843217],
+            [0.8560456003452277, -0.5169003096628776],
+            [0.9520748400258878, -0.30586516472079656]
+    ])
 def dictionary(symbol):
     return{
             #"0": "537642",
@@ -139,14 +156,119 @@ def position(character):
                 #    "undefined7" : [,-198],
     }.get(character,[0,0])
 
+'''
+@prag: ['key1', 'key2', ...]
+@return: a five-element list containing top 5 results
+this function is to give the top 5 prediction results
+'''
+def predict(input_seq, number = 5):
+    length = len(symbols_tobe_matched)
+    losses = np.zeros([1,length])
+    vectors = divide_single_vector(remove_duplicate( input_seq ))
+    for i in range(length):
+        min_loss_for_one_symbol = float("inf")
+        for j in range(len(dictionary(symbols_tobe_matched[i]))):
+
+            loss, _ = dtw(vectors, dictionary(symbols_tobe_matched[i])[j], dist = get_possibility)
+            if loss <= min_loss_for_one_symbol:
+                min_loss_for_one_symbol = loss
+        #losses[i], _ = dtw(vectors, dictionary(symbols_tobe_matched[i]), dist=get_possibility)
+        losses[0][i] = min_loss_for_one_symbol
+
+    _sorted = np.argsort(losses)
+    top_number_result=[]
+    for i in range(number):
+        top_number_result.append(symbols_tobe_matched[_sorted[0][i]])
+
+    return top_number_result
+
+'''
+This function is to remove duplicate keys in the inpue sequence
+like  'a' 'b' 'c' 'c' 'd' will be shortened into 'a' 'b' 'c' 'd' 
+'''
+def remove_duplicate(input_seq):
+    duplicate_removed = []
+    for i in range(len(input_seq)):
+        if i == 0:
+            duplicate_removed.append(input_seq[i])
+        else:
+            if input_seq[i] != input_seq[i-1]:
+                duplicate_removed.append(input_seq[i])
+    return duplicate_removed
+
 def get_possibility(first, second):
     temp = possibilities_matrix[int(first)][int(second)]
     if temp == 0:
+        #return float("inf")
         return 999999
     else:
         return -math.log(temp)
 
 def get_vector(one, two):
     x = position(two)[0] - position(one)[0]
-    y = position(two)[0] - position(one)[0]
+    y = position(two)[1] - position(one)[1]
     return [x, y]
+
+
+
+
+
+'''
+@prag: ['key1', 'key2', ...]
+@return: ['1','2',...]
+pass in an input list, which contains all the keys pressed during the gliding
+parse it into a list of integers and every integer stands for a vector among the 12 vectors
+'''
+def divide_single_vector(input_seq):
+    parsed_vectors = []
+    for index in range(len(input_seq) -1):
+        normalize_vector = normalize(get_vector(input_seq[index], input_seq[index+1]))
+        parsed_vectors.append(map_to_centroids(normalize_vector))
+    return parsed_vectors
+
+
+
+''' 
+@prag: [x,y]
+@return: vector i 
+pass in a list with two characters
+return the closest vector to represent the input 
+if the input contains space,
+the space should map to the exact location among the 7 space keys
+'''
+def map_to_centroids(list):
+
+    distances = euclidean_distance(np.array(list), clustered_centroids)
+    closest_i = np.argmin(distances)
+    return closest_i
+
+def euclidean_distance(one_sample, X):
+    one_sample = one_sample.reshape(1, -1)
+    X = X.reshape(X.shape[0], -1)
+    distances = np.power(np.tile(one_sample, (X.shape[0], 1)) - X, 2).sum(axis=1)
+    return distances
+
+'''
+@prag: [x,y]
+@return: normalized [_x, _y]
+pass in a list, which contains two elements,
+return the normalized list
+'''
+def normalize(list):
+    x = list[0]
+    y = list[1]
+    _sum = (x*x + y*y) ** 0.5
+    _x = x / _sum
+    _y = y / _sum
+    return [_x, _y]
+
+if __name__ =="__main__":
+    test = list("8ujjjjjm")
+    
+
+    print(remove_duplicate(test))
+    #print(divide_single_vector(test))
+    #print(predict(list("8uhbvfrtyjm")))
+    #a ,b = dtw([8, 8, 8, 5, 2, 2, 6, 6, 10, 9],['4', '6', '7', '5', '7', '6', '4'], dist = get_possibility)
+    #a ,b = dtw([8, 8, 8, 5, 2, 2, 6, 6, 10, 9],['4', '6', '7', '5', '7', '6', '4'], dist = get_possibility)
+    #print(a,b)
